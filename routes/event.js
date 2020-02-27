55a74647ee757c83d7e94a6f9938b630fdfb8ff8
MehmetCan75 <mehmetozmenfb@gmail.com>
2020-02-27 11:49:23 +0100

    createUser

--- routes/event.js
+++ routes/event.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = new express.Router();
+const EventModel = require("../models/Event");
 
 
 router.get("/", (req, res, next) => {
@@ -13,17 +14,31 @@ router.get("/:id", (req, res, next) => {
 });
 
 
+router.post("/create", (req, res, next) => {
+    const newEvent = {...req.body}
+    newEvent.creator = req.session.currentUser._id
 
-router.post("/", (req, res, next) => {
-    res.status(200).json({ msg: "@todo" })
+    EventModel.create(newEvent)
+    .then((results) => {
+        res.status(200).json({ msg: "OK"})
+    }).catch(err => {
+        res.status(500).json(err)
+    })
+
 });
-
 
 
 router.patch("/:id", (req, res, next) => {
-    res.status(200).json({ msg: "@todo" })
-});
+    const Eventvalues = {...req.body}
 
+    EventModel.findByIdAndUpdate(req.params.id, Eventvalues)
+    .then((results) => {
+          res.status(200).json({ msg: "OK" })
+    }).catch(err => {
+    res.status(500).json(err)
+    })
+
+});
 
 
 router.delete("/:id", (req, res, next) => {

e6866f3e4d42e35e5ba281721d2bb57ee270cc62
Baptiste WARGNY <bwargny92@hotmail.com>
2020-02-28 18:18:27 +0100

    user update

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = new express.Router();
 const userModel = require("../models/User");
+const uploader = require("./../config/cloudinary");
 
 
 /* GET users listing. */
@@ -17,14 +18,43 @@ router.get("/", (req, res, next) => {
 
 
 
-router.get("/:id", (req, res, next) => {
-    res.status(200).json({ msg: "@todo" })
-});
+router.get("/:id", async (req, res, next) => {
+    try {
+        res.json(await userModel.findById(req.params.id));
+      } catch (dbErr) {
+        next(dbErr);
+      }
+    });
 
 
 
-router.patch("/", (req, res, next) => {
-    res.status(200).json({ msg: "@todo" })
+router.patch("/:id",uploader.single("avatar"), (req, res, next) => {
+    const { email, firstName, lastName, city, 
+        // password,
+        username,userId } = req.body;
+        console.log(req.body)
+  const updateUser = {
+    email, 
+    firstName,
+    lastName,
+    city,
+    username,
+    // password,
+    
+  };
+
+  if (req.file) updateUser.avatar = req.file.secure_url;
+
+  
+  userModel
+    .findByIdAndUpdate(userId, updateUser, {new: true})
+    .then(dbRes => {
+      res.status(200).json(dbRes);
+    })
+    .catch(err => {
+      console.log("patch error", err);
+      res.status(500).json(err);
+    });
 });
 
 

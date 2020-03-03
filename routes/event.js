8ec8de1ed8377429e870a76fc7e92db3ac9cac8f
colinevel <coline.velard@gmail.com>
2020-03-03 16:27:14 +0100

    dashboard route

--- routes/event.js
+++ routes/event.js
@@ -15,6 +15,18 @@ router.get("/events", (req, res, next) => {
     .catch(next);});
 
 
+router.get("/dashboard", (req, res, next) => {
+// var idUsers = mongoose.Types.ObjectId(users);
+    EventModel
+    .find({participants:{ $in: [req.user._id] }})
+    .populate("sport")
+    .populate("user")
+    .then(dbRes => {
+    console.log("this is my dbres", dbRes);
+    res.status(200).json({ events: dbRes })})
+    .catch(next);
+});
+
 
 router.get("/events/:id", (req, res, next) => {
     EventModel.findById(req.params.id)
